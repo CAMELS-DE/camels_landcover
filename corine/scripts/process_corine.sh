3df5fe1f570a1ec54bc9607e4b6e248d42717bb8
AlexDo1 <alexander.dolich@kit.edu>
2024-03-04 10:02:38 +0100

    Add start processing message to script

--- corine/scripts/process_corine.sh
+++ corine/scripts/process_corine.sh
@@ -5,6 +5,9 @@ mkdir -p /output_data/scripts
 # logging
 exec > >(tee -a /output_data/scripts/processing.log) 2>&1
 
+# Start processing
+echo "[$(date +%F\ %T)] Starting processing of CORINE land-cover data for the CAMELS-DE dataset..."
+
 # Generate catchments geopackage
 echo "[$(date +%T)] Generating MERIT Hydro catchments geopackage..."
 python /scripts/01_generate_catchments_gpkg.py
